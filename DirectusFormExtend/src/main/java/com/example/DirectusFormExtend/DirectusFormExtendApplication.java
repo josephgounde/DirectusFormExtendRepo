63baddcfb262b567dba6a9b1153fe5561ce50dc7
Gounde Talla Wafo Joseph <djopiano@gmail.com>
2025-09-03 09:30:15 +0100

    mail raw data send

---
+++ DirectusFormExtend/src/main/java/com/example/DirectusFormExtend/DirectusFormExtendApplication.java
@@ -0,0 +1,13 @@
+package com.example.DirectusFormExtend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectusFormExtendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectusFormExtendApplication.class, args);
+	}
+
+}
---
+++ DirectusFormExtend/src/test/java/com/example/DirectusFormExtend/DirectusFormExtendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DirectusFormExtend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectusFormExtendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
